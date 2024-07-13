559cb47695bdbde3b592a8db72a62ec303fb3008
leticea <nascimento.leticeah@gmail.com>
2024-07-13 14:17:59 -0300

    feat(app.tsx): creating guests modal functions

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,8 @@ import {
   UserRoundPlus,
   Settings2,
   X,
+  AtSign,
+  Plus,
 } from "lucide-react";
 import { useState } from "react";
 
@@ -122,13 +124,67 @@ export function App() {
 
       {isGuestsModalOpen && (
         <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900">
-            <div className="flex items-center justify-between">
-              <h2>Select guests</h2>
-              <button type="button" onClick={closeGuestsModal}>
-                <X className="size-5 text-zinc-400" />
-              </button>
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Select guests</h2>
+                <button type="button" onClick={closeGuestsModal}>
+                  <X className="size-5 text-zinc-400" />
+                </button>
+              </div>
+              <p className="text-sm text-zinc-400">
+                Guests will receive emails to confirm their participation in the
+                trip.
+              </p>
             </div>
+
+            <div className="flex flex-wrap gap-2">
+              <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
+                <span className="text-zinc-300">jessica.white44@yahoo.com</span>
+                <button type="button">
+                  <X className="size-4 text-zinc-400" />
+                </button>
+              </div>
+
+              <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
+                <span className="text-zinc-300">jessica.white44@yahoo.com</span>
+                <button type="button">
+                  <X className="size-4 text-zinc-400" />
+                </button>
+              </div>
+
+              <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
+                <span className="text-zinc-300">jessica.white44@yahoo.com</span>
+                <button type="button">
+                  <X className="size-4 text-zinc-400" />
+                </button>
+              </div>
+
+              <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
+                <span className="text-zinc-300">jessica.white44@yahoo.com</span>
+                <button type="button">
+                  <X className="size-4 text-zinc-400" />
+                </button>
+              </div>
+            </div>
+
+            <div className="w-full h-px bg-zinc-800" />
+
+            <form className="p-2.5 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+              <div className="px-2 flex items-center flex-1 gap-2">
+                <AtSign className="size-5 text-zinc-400" />
+                <input
+                  type="text"
+                  placeholder="Enter the guest's email"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+
+              <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+                Invite
+                <Plus className="size-5" />
+              </button>
+            </form>
           </div>
         </div>
       )}
