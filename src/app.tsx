38df54adddfd878477a1e2c2c9236063ee63b76a
leticea <nascimento.leticeah@gmail.com>
2024-09-08 11:12:37 -0300

    feat(app.tsx): creating trip confirmation form

--- src/app.tsx
+++ src/app.tsx
@@ -7,6 +7,8 @@ import {
   X,
   AtSign,
   Plus,
+  User,
+  Mail,
 } from "lucide-react";
 import { FormEvent, useState } from "react";
 
@@ -215,6 +217,59 @@ export function App() {
           </div>
         </div>
       )}
+
+      <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+        <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+          <div className="space-y-2">
+            <div className="flex items-center justify-between">
+              <h2 className="text-lg font-semibold">Confirm trip creation</h2>
+              <button type="button" onClick={closeGuestsModal}>
+                <X className="size-5 text-zinc-400" />
+              </button>
+            </div>
+            <p className="text-sm text-zinc-400">
+              To complete the creation of the trip to{" "}
+              <span className="font-semibold text-zinc-100">
+                Florianópolis, Brazil
+              </span>{" "}
+              on the dates of{" "}
+              <span className="font-semibold text-zinc-100">
+                August 16th to 27th, 2024
+              </span>
+              , fill in your details below:
+            </p>
+          </div>
+
+          <form onSubmit={addNewEmailToInvite} className="space-y-3">
+            <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+              <User className="size-5 text-zinc-400" />
+              <input
+                name="name"
+                placeholder="Your full name"
+                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+              />
+            </div>
+
+            <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+              <Mail className="size-5 text-zinc-400" />
+              <input
+                type="email"
+                name="email"
+                placeholder="Your personal email"
+                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="bg-lime-300 text-lime-950 rounded-lg w-full px-5 h-11 font-medium flex items-center justify-center gap-2 hover:bg-lime-400"
+            >
+              Invite
+              <Plus className="size-5" />
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
