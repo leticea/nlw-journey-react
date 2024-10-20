7c2a09144247e491f89eda95f0fbcf8b5ef2dbe1
leticea <nascimento.leticeah@gmail.com>
2024-10-20 18:26:38 -0300

    feat(trip-details/index.tsx): creating register activity modal

--- src/pages/trip-details/index.tsx
+++ src/pages/trip-details/index.tsx
@@ -3,13 +3,29 @@ import {
   CircleCheck,
   CircleDashed,
   Link2,
+  Mail,
   MapPin,
   Plus,
   Settings2,
+  Tag,
+  User,
   UserCog,
+  X,
 } from "lucide-react";
+import { useState } from "react";
 
 export function TripDetailsPage() {
+  const [isCreateActivityModalOpen, setIsCreateActivityModalOpen] =
+    useState(false);
+
+  function openCreateActivityModal() {
+    setIsCreateActivityModalOpen(true);
+  }
+
+  function closeCreateActivityModal() {
+    setIsCreateActivityModalOpen(false);
+  }
+
   return (
     <div className="max-w-6xl px-6 py-10 mx-auto space-y-8">
       <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
@@ -37,7 +53,10 @@ export function TripDetailsPage() {
         <div className="flex-1 space-y-6">
           <div className="flex items-center justify-between">
             <h2 className="text-3xl font-semibold">Activities</h2>
-            <button className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400">
+            <button
+              onClick={openCreateActivityModal}
+              className="bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400"
+            >
               <Plus className="size-5" />
               Register activity
             </button>
@@ -160,6 +179,52 @@ export function TripDetailsPage() {
           </div>
         </div>
       </main>
+
+      {isCreateActivityModalOpen && (
+        <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+            <div className="space-y-2">
+              <div className="flex items-center justify-between">
+                <h2 className="text-lg font-semibold">Register activity</h2>
+                <button type="button" onClick={closeCreateActivityModal}>
+                  <X className="size-5 text-zinc-400" />
+                </button>
+              </div>
+              <p className="text-sm text-zinc-400">
+                All guests can view the activities.
+              </p>
+            </div>
+
+            <form className="space-y-3">
+              <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+                <Tag className="size-5 text-zinc-400" />
+                <input
+                  name="title"
+                  placeholder="What is the activity?"
+                  className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+                />
+              </div>
+
+              {/* <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+              <Mail className="size-5 text-zinc-400" />
+              <input
+                type="email"
+                name="email"
+                placeholder="Your personal email"
+                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+              />
+            </div> */}
+
+              <button
+                type="submit"
+                className="bg-lime-300 text-lime-950 rounded-lg w-full px-5 h-11 font-medium flex items-center justify-center gap-2 hover:bg-lime-400"
+              >
+                Save activity
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
