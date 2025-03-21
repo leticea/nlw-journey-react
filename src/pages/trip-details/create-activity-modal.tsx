685d19fb3166f7fe57db324375a7c52435dbf74c
leticea <nascimento.leticeah@gmail.com>
2025-03-21 17:23:01 -0300

    feat(create-activity-modal.tsx): getting data from the activity creation modal

--- src/pages/trip-details/create-activity-modal.tsx
+++ src/pages/trip-details/create-activity-modal.tsx
@@ -1,5 +1,8 @@
-import { X, Tag, Calendar } from "lucide-react";
+import { Calendar, Tag, X } from "lucide-react";
 import { Button } from "../../components/button";
+import { FormEvent } from "react";
+import { api } from "../../lib/axios";
+import { useParams } from "react-router-dom";
 
 interface CreateActivityModalProps {
   closeCreateActivityModal: () => void;
@@ -8,24 +11,46 @@ interface CreateActivityModalProps {
 export function CreateActivityModal({
   closeCreateActivityModal,
 }: CreateActivityModalProps) {
+  const { tripId } = useParams();
+
+  async function createActivity(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+
+    const title = data.get("title")?.toString();
+    const occurs_at = data.get("occurs_at")?.valueOf();
+
+    await api.post(`/trips/${tripId}/activities`, {
+      title,
+      occurs_at,
+    });
+
+    closeCreateActivityModal();
+  }
+
   return (
     <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
       <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
         <div className="space-y-2">
           <div className="flex items-center justify-between">
-            <h2 className="text-lg font-semibold">Register activity</h2>
-            <button type="button" onClick={closeCreateActivityModal}>
-              <X className="size-5 text-zinc-400" />
+            <h2 className="font-lg font-semibold">Register activity</h2>
+            <button>
+              <X
+                className="size-5 text-zinc-400"
+                onClick={closeCreateActivityModal}
+              />
             </button>
           </div>
+
           <p className="text-sm text-zinc-400">
-            All guests can view the activities.
+            All guests can view activities.
           </p>
         </div>
 
-        <form className="space-y-3">
+        <form onSubmit={createActivity} className="space-y-3">
           <div className="h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-            <Tag className="size-5 text-zinc-400" />
+            <Tag className="text-zinc-400 size-5" />
             <input
               name="title"
               placeholder="What is the activity?"
@@ -33,21 +58,17 @@ export function CreateActivityModal({
             />
           </div>
 
-          <div className="flex items-center gap-2">
-            <div className="flex-1 h-14 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
-              <Calendar className="size-5 text-zinc-400" />
-              <input
-                type="datetime-local"
-                name="occurs_at"
-                placeholder="Date and time of the activity"
-                className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
-              />
-            </div>
+          <div className="h-14 flex-1 px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2">
+            <Calendar className="text-zinc-400 size-5" />
+            <input
+              type="datetime-local"
+              name="occurs_at"
+              placeholder="Date and time of activity"
+              className="bg-transparent text-lg placeholder-zinc-400 outline-none flex-1"
+            />
           </div>
 
-          <Button variant="primary" size="full">
-            Save activity
-          </Button>
+          <Button size="full">Save Activity</Button>
         </form>
       </div>
     </div>
