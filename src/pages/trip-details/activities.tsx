0a10dea9554b1fb2b952ed279ae7e034aef34ba9
leticea <nascimento.leticeah@gmail.com>
2025-03-24 22:17:01 -0300

    feat(activities.tsx): formatting the time

--- src/pages/trip-details/activities.tsx
+++ src/pages/trip-details/activities.tsx
@@ -25,20 +25,38 @@ export function Activities() {
 
   return (
     <div className="space-y-8">
-      {activities.map((activity) => {
+      {activities.map((category) => {
         return (
-          <div key={activity.date} className="space-y-2.5">
+          <div key={category.date} className="space-y-2.5">
             <div className="flex gap-2 items-baseline">
               <span className="text-xl text-zinc-300 font-semibold">
-                Day {format(activity.date, "d")}
+                Day {format(category.date, "d")}
               </span>
               <span className="text-xs text-zinc-500">
-                {format(activity.date, "EEEE")}
+                {format(category.date, "EEEE")}
               </span>
             </div>
-            <p className="text-sm text-zinc-500">
-              No activity registered on that date
-            </p>
+            {category.activities.length > 0 ? (
+              <div>
+                {category.activities.map((activity) => {
+                  return (
+                    <div key={activity.id} className="space-y-2.5">
+                      <div className="px-4 py-2.5 bg-zinc-900 rounded-xl shadow-shape flex items-center gap-3">
+                        <CircleCheck className="size-5 text-lime-300" />
+                        <span className="text-zinc-100">{activity.title}</span>
+                        <span className="text-zinc-400 text-sm ml-auto">
+                          {format(activity.occurs_at, "p")}
+                        </span>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            ) : (
+              <p className="text-sm text-zinc-500">
+                No activity registered on that date
+              </p>
+            )}
           </div>
         );
       })}
